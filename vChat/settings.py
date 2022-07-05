11ea6e06d78b10c49228bd577681fda2f8510f26
Konstantin Shambazov <k.shambazov@gmail.com>
2022-07-05 13:51:34 +0900

    chore: update requirements

--- vChat/settings.py
+++ vChat/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
-from email.mime import base
+import django_heroku
+django_heroku.settings(locals())
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
